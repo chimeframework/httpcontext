2493411319abd4095fb9e833df311d44137bb2f9
Ashok Gelal <ashokgelal@gmail.com>
2011-11-15 01:00:32 -0700

    Modify request to take a raw httprequest

--- src/request.go
+++ src/request.go
@@ -1,11 +1,15 @@
 package httpcontext
 
+import "net/http"
+
 type Request struct {
+    *http.Request
     attributes map[string]string
 }
 
-func NewRequest() *Request {
+func NewRequest(request *http.Request) *Request {
     this := &Request{}
+    this.Request = request
     this.attributes = make(map[string]string)
     return this
 }
